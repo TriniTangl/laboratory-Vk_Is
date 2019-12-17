6bf3443b04e07cfa2dc6b9804eb362ec9eec3f1c
Vladyslav Havryliuk <vhavryliuk@corevalue.net>
2019-12-17 18:32:33 +0200

    завершена ргр / почав робити звіт

--- P&RWebS/rgr/src/app.ts
+++ P&RWebS/rgr/src/app.ts
@@ -1,11 +1,11 @@
-import express from 'express';
 import nodeCmd from 'node-cmd';
+import express, {Express} from 'express';
 
-const app = express();
-const port = 3000;
-const jsonParser = express.json();
+const app: Express = express();
+const port: number = 3000;
+const jsonParser: any = express.json();
 
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT,    PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
@@ -13,14 +13,16 @@ app.use(function (req, res, next) {
     next();
 });
 app.post('/rgr', jsonParser, (req, res) => {
-    console.log(req.body);
     if (req.body) {
         nodeCmd.get(req.body.command, (error, data, stderr) => {
-            res.json({
-                result: data,
-                error: error
+            nodeCmd.get('cd', (errorDir, dataDir, stderrDir) => {
+                res.json({
+                    dir: dataDir,
+                    result: data,
+                    error: error
+                });
             });
-        })
+        });
     } else {
         return res.status(401).json({message: `Doesn't get any data`});
     }
@@ -29,5 +31,5 @@ app.listen(port, err => {
     if (err) {
         return console.log(err);
     }
-    return console.log(`server is listening on port:${port}`)
+    return console.log(`Server is listening on port:${port}`)
 });
